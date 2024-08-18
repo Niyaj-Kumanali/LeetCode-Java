f912f3a02d3f4b4ff2ed313ac902786a80c9162c
Niyaj-Kumanali <iamniyazahmad777@gmail.com>
2024-08-18 14:52:02 +0530

    'updates

--- LargestRectangleInHistogram.java
+++ LargestRectangleInHistogram.java
@@ -4,6 +4,26 @@ package LeetCode;
 
 public class LargestRectangleInHistogram {
     public static void main(String[] args) {
-        System.out.println(Math.log10(0)+1);
+
+    }
+
+    public static int largestRectangleArea(int[] heights) {
+        int res = 0;
+        for (int i=0; i< heights.length; i++){
+            int max = 0;
+            int element = heights[i];
+            for (int j = i; j < heights.length; j++) {
+                max += element;
+                if(heights[j] < element)
+                    break;
+            }
+
+            for (int j = 0; j < i; j++) {
+
+            }
+            if (res < max)
+                res = max;
+        }
+        return 0;
     }
 }
